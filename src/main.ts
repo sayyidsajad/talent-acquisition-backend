b0bc41c22e8e5159ea8333617d73e839717a4ef5
Sayyid Sajad <sayyidsajada4s@gmail.com>
2025-03-25 08:14:29 +0530

    removed firebase updates

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as functions from 'firebase-functions';
+
+let server;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const clientHost = configService.get('CLIENT_HOST');
+
+  const clientHost = configService.get('CLIENT_HOST') || '*';
   app.enableCors({
     origin: [clientHost],
     methods: '*',
     allowedHeaders: '*',
     credentials: true,
   });
-  await app.listen(3000);
+
+  await app.init();
+
+  const expressInstance = app.getHttpAdapter().getInstance();
+  server = functions.https.onRequest(expressInstance);
 }
-bootstrap();
+
+bootstrap().catch(console.error);
+
+export { server };
