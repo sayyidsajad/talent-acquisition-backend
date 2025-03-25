af79a15f9d0777bd5aa29b2e622d25af28af2470
Sayyid Sajad <sayyidsajada4s@gmail.com>
2025-03-25 11:15:37 +0530

    database url updated

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
   const clientHost = configService.get<string>('CLIENT_HOST');
